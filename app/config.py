a5bbaed057e6aed7de11cd4c84c42af52c8fb796
alessiaSMM <152690456+alessiaSMM@users.noreply.github.com>
2025-11-30 23:12:14 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,4 +7,7 @@ KMS_KEY_NAME     = os.getenv("KMS_KEY_NAME", "")
 KMS_KEY_VERSION  = os.getenv("KMS_KEY_VERSION", "1")
 
 RPC_URL          = os.getenv("RPC_URL", "")
-CHAIN_ID         = int(os.getenv("CHAIN_ID", "137"))  # 137 = Polygon mainnet
+CHAIN_ID         = int(os.getenv("CHAIN_ID", "137"))
+
+# variabile che contiene il JSON del servizio di Google Cloud Platform
+GCP_SA_JSON      = os.getenv("GCP_SA_JSON", "")
